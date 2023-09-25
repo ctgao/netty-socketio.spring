8f6ac943299e3cc17ce95c9d4d6bba44e93e2d53
ctgao <137011200+ctgao@users.noreply.github.com>
2023-09-25 17:29:42 -0400

    fixed the styling

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import './ChatClient.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{display: 'inline-block'}}>
-          Combining Socket.io and React <img src={logo} className="App-logo" alt="logo" />
-        </div>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <div style={{display: 'inline-block'}}>
+            Combining Socket.io and React <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </header>
+      </div>
+      <div className="MainBody">
+        <h1>Chat Client</h1>
+        <div id="console" class="well"></div>
+
+        <form className="well form-inline" onsubmit="return false;">
+          <input id="msg" class="input-xlarge" type="text" placeholder="Type something..."/>
+          <button type="button" class="btn" id="send">Send</button>
+          <button type="button" class="btn">Disconnect</button>
+        </form>
+      </div>
     </div>
   );
 }
