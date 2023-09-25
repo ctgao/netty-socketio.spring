e8acd869e060da99a6ae0210b7d643568e06f109
ctgao <137011200+ctgao@users.noreply.github.com>
2023-09-25 16:43:44 -0400

    trying to recreate the front-end in react

---
+++ react-client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{display: 'inline-block'}}>
+          Combining Socket.io and React <img src={logo} className="App-logo" alt="logo" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
