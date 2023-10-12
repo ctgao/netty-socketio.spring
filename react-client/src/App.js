758ac9b1735e3f3071aa88f090365a12338b10b0
ctgao <137011200+ctgao@users.noreply.github.com>
2023-10-12 17:12:57 -0400

    saving

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,15 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import './ChatClient.css';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:9092';
+
+export const socket = io(URL, {
+  autoConnect: false
+});
 
 function App() {
-  var userName = 'user' + Math.floor((Math.random() * 1000) + 1);
-  var socket = io('http://localhost:9092/chat?token=abc123', {
-    transports: ['polling', 'websocket']
-  });
-  socket.on('connect', function () {
-    output('<span class="connect-msg">The client has connected with the server. Username: ' + userName + '</span>');
-  });
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      };
+  }, []);
+
+  const sendConnect = () => {
+    console.log("connecting");
+    socket.connect();
+  }
+
+  const sendDisconnect = () => {
+    console.log("disconnecting");
+    socket.disconnect();
+  }
 
   return (
     <div>
@@ -22,12 +53,13 @@ function App() {
       </div>
       <div className="MainBody">
         <h1>Chat Client</h1>
-        <div id="console" class="well"></div>
+        <div id="console" className="well">{isConnected ? "AM CONNECTED" : "not connected"}</div>
 
-        <form className="well form-inline" onsubmit="return false;">
-          <input id="msg" class="input-xlarge" type="text" placeholder="Type something..."/>
-          <button type="button" class="btn" id="send">Send</button>
-          <button type="button" class="btn">Disconnect</button>
+        <form className="well form-inline">
+          <input id="msg" className="input-xlarge" type="text" placeholder="Type something..."/>
+          <button type="button" className="btn" id="send">Send</button>
+          <button type="button" className="btn" onClick={sendConnect}>Connect</button>
+          <button type="button" className="btn" onClick={sendDisconnect}>Disconnect</button>
         </form>
       </div>
     </div>
